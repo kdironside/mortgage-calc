094a334e5136a2c6d13dc57f2502cdabdee3822c
KD Ironside <kdironside@gmail.com>
2012-06-02 20:11:03 -0500

    Calculation working.

--- lib/mort_calc/mcalculator.rb
+++ lib/mort_calc/mcalculator.rb
@@ -4,7 +4,7 @@ require 'flt'
 module MortCalc
 
   class Calculator 
-    attr_accessor :user_input, :status, :amount, :interest, :term_years, :monthly_payment
+    attr_accessor :user_input, :status, :amount, :interest, :term_years, :months, :monthly_payment
     include Flt
     include Validator
     extend Validator
@@ -35,18 +35,25 @@ module MortCalc
       return input_value[1]
     end
     
-    def calculate_payment(amount, interest, term_years)
-      @output.puts 'Calculating...'
+    # The formula below calculates M, the monthly mortgage payment:
+    # M = P [ i(1 + i)n ] / [ (1 + i)n - 1]  where P = prinicpal @amount, 
+    #                                              i = @interest per month (compounding period)
+    #                                          and n = number of compounding periods (number of @months)
+    
+    def calculate_payment(amt, int, yrs) 
+      @output.puts 'Calculating...........................' 
+      int = int / 100 / 12                          # e.g. 5% => .05 / 12 = 0.00416666666667
+      months = yrs * 12                             # e.g. 30 yrs => 360 months
+      monthly_payment = amt * (int * ((1 + int)**months)) / (((1 + int)**months) - 1)        
     end    
   
     def finish(monthly_payment)
-      @output.puts "Your monthly payment is: monthly_payment"
+      @output.puts "Your monthly payment is:" + "\n$%.2f per month" % [monthly_payment] 
     end 
     
     def quit
       @output.puts "Oops, no value entered...exiting..."
       exit
-    end    
-    
+    end       
   end
 end
